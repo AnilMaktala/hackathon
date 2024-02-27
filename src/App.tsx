047078d3f4324a87b13ce7295c278095bb8a7687
Anil Maktala <amaktala@amazon.com>
2024-02-27 12:58:53 -0500

    lib upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import React, { useState, useEffect } from "react";
 import "@aws-amplify/ui-react/styles.css";
 import { signOut } from 'aws-amplify/auth';
 import TodoList from "./components/TodoList";
 
 function App() {
 
+  async function handleSignOut() {
+  try {
+    await signOut();
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+  }
 
   return (
     <>
-      <h1>Hello, Amplify 👋</h1>
-       <TodoList />
+      <html>
+        <header>
+          <h1>Hello, Amplify 👋 </h1>
+          <button title= onClick={handleSignOut}></button>
+        </header>
+        <body>
+
+
+        </body>
+      </html>
     </>
   );
 }
