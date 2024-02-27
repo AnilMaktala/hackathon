fd73003a86e375381b60ae5d14b5727d3797d5cb
Anil Maktala <amaktala@amazon.com>
2024-02-27 11:03:37 -0500

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { signOut } from 'aws-amplify/auth';
 import TodoList from "./components/TodoList";
+
 function App() {
+  async function handleSignOut() {
+  try {
+    await signOut();
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+}
   return (
     <>
       <h1>Hello, Amplify 👋</h1>
